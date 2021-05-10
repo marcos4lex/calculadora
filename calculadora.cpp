5514bea717c61963df9adaba1d1ab209cc82521d
Marcos Almeida <80858006+marcos4lex@users.noreply.github.com>
2021-05-10 18:48:28 -0300

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -5,19 +5,21 @@ int main()
 {
     int num1, num2, soma, sub, mult, divi;
 
-    printf("Calculadora 0.5\n");
-    printf("Entre os 2 numeros a serem processados: ");
-    scanf("%i%i", &num1, &num2);
+    printf("\t\tCalculadora 0.6\n\n");
+    printf("Entre com o primeiro numero a ser processado: ");
+    scanf("%d", &num1);	
+    printf("Entre com o segundo numero a ser processado: ");
+    scanf("%d", &num2);
 
     soma       = num1 + num2;
     sub        = num1 - num2;
     mult       = num1 * num2;
     divi       = num1 / num2;
 
-    printf( "A soma e: %i\n", soma );
-    printf( "A subtracao e: %i\n", sub );
-    printf( "A multiplicacao e: %i \n", mult );
-    printf( "A divisao e: %i \n", divi );
+    printf( "A soma e: %d\n", soma );
+    printf( "A subtracao e: %d\n", sub );
+    printf( "A multiplicacao e: %d \n", mult );
+    printf( "A divisao e: %d \n", divi );
 
     return 0;
 }
