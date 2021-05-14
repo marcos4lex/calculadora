d3c2b5b6719c7b1707569c2ef8a3e0ad0536b7d4
Marcos Almeida <80858006+marcos4lex@users.noreply.github.com>
2021-05-14 20:44:03 -0300

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,17 +3,19 @@
 #include <conio.h>
 #include <locale.h>
 
-int soma(int soma1, int soma2);
-int subtracao(int sub1, int sub2);
-int multiplicacao(int mult1, int mult2);
-int divisao(int div1, int div2);
+float soma(float soma1, float soma2);
+float subtracao(float sub1, float sub2);
+float multiplicacao(float mult1, float mult2);
+float divisao(float div1, float div2);
 int restdiv(int restdiv1, int restdiv2);
-int fatorial(int numfat);
-int potencia(int base, int expoente);
-int porcent(int porc1, int porc2);
+float fatorial (float numfat);
+float potencia(float base, int expoente);
+float porcent (float porc1, float porc2);
 
 int main(){									/* Área destinada ao Menu da calculadora */
-	int opcoes, num1, num2, numfato, bas, expo, resultado; 
+
+	float resultado, num1, num2, numfato, bas, expo;
+	int opcoes;
 	setlocale(LC_ALL, "");
 	
 	printf ("\t\t\t\tSeja bem vindo a calculadora do Marcos!\n\n");
@@ -34,83 +36,83 @@ int main(){									/* Área destinada ao Menu da calculadora */
 		case 1: 
 			printf ("A operação escolhida foi ADIÇÃO!\n");
 			printf ("Insira o primeiro número:\n");
-			scanf ("%d", &num1);
+			scanf ("%f", &num1);
 			printf ("Insira o segundo número:\n");
-			scanf ("%d", &num2);
+			scanf ("%f", &num2);
 			resultado = soma (num1, num2);
-			printf ("O resultado da operação é: %d\n", resultado);
+			printf ("O resultado da operação é: %0.2f\n", resultado);
 			break;
  
 		case 2:
 			printf ("A operação escolhida foi SUBTRAÇÃO!\n");
 			printf ("Insira o primeiro número:\n");
-			scanf ("%d", &num1);
+			scanf ("%f", &num1);
 			printf ("Insira o segundo número:\n");
-			scanf ("%d", &num2);
+			scanf ("%f", &num2);
 			resultado = subtracao (num1, num2);
-			printf ("O resultado da operação é: %d\n", resultado);
+			printf ("O resultado da operação é: %0.2f\n", resultado);
 			break;
  
 		case 3:
 			printf ("A operacao escolhida foi MULTIPLICAÇÃO!\n");
 			printf ("Insira o primeiro número:\n");
-			scanf ("%d", &num1);
+			scanf ("%f", &num1);
 			printf ("Insira o segundo número:\n");
-			scanf ("%d", &num2);
+			scanf ("%f", &num2);
 			resultado = multiplicacao (num1, num2);
-			printf ("O resultado da operação é: %d\n", resultado);
+			printf ("O resultado da operação é: %0.2f\n", resultado);
 			break;
- 
+			
 		case 4:
   			printf ("A operação escolhida foi DIVISAO!\n");
   			printf ("Insira o primeiro número:\n");
-  			scanf ("%d", &num1);
+  			scanf ("%f", &num1);
   			printf ("Insira o segundo número:\n");
-  			scanf ("%d", &num2);
+  			scanf ("%f", &num2);
   
   			if ( num1 == 0 || num2 == 0) {
       			printf ("Não tem como dividir por zero né champz!!");}
 			else {
     			resultado = divisao (num1, num2);
-    			printf ("O resultado da operação é: %d\n", resultado);}
+    			printf ("O resultado da operação é: %0.2f\n", resultado);}
   			break;
-		
-		case 5: 
+  			
+  		case 5: 
 			printf ("A operação escolhida foi RESTO DA DIVISÃO!\n");
 			printf ("Insira o primeiro número:\n");
-			scanf ("%d", &num1);
+			scanf ("%f", &num1);
 			printf ("Insira o segundo número:\n");
-			scanf ("%d", &num2);
+			scanf ("%f", &num2);
 			resultado = restdiv (num1, num2);
-			printf ("O resultado da operação é: %d\n", resultado);
+			printf ("O resultado da operação é: %0.2f\n", resultado);
 			break;
- 
+			
 		case 6:
 			printf ("A operação escolhida foi FATORIAL!\n");
 			printf ("Insira o número a ter sua fatorial calculada:\n");
-			scanf("%d", &numfato);
+			scanf("%f", &numfato);
 			resultado = fatorial (numfato);
-			printf ("O resultado da operação é: %d\n", resultado);
+			printf ("O resultado da operação é: %0.2f\n", resultado);
 			break;
- 
+		
 		case 7:
 			printf ("A operacao escolhida foi POTENCIAÇÃO!\n");
 			printf ("Insira a base:\n");
-			scanf ("%d", &bas);
+			scanf ("%f", &bas);
 			printf ("Insira o expoente:\n");
-			scanf ("%d", &expo);
+			scanf ("%f", &expo);
 			resultado = potencia (bas, expo);
-			printf("O resultado da operação é: %d\n", resultado);
+			printf("O resultado da operação é: %.2f\n", resultado);
 			break;
 			
 		case 8:
 			printf ("A operação escolhida foi PORCENTAGEM!\n");
 			printf ("Insira o primeiro valor:\n");
-			scanf ("%d", &num1);
+			scanf ("%f", &num1);
 			printf ("Insira o segundo valor:\n");
-			scanf ("%d", &num2);
+			scanf ("%f", &num2);
 			resultado = porcent (num1, num2);
-			printf("O resultado da operação é: %d\n", resultado);
+			printf("O resultado da operação é: %0.2f\n", resultado);
 			break;
  
 		default:
@@ -118,23 +120,23 @@ int main(){									/* Área destinada ao Menu da calculadora */
 	}
 }
 
-int soma (int soma1, int soma2) {						/* Área destinada a soma */
-	int resultado;
+float soma (float soma1, float soma2) {						/* Área destinada a soma */
+	float resultado;
 	resultado = soma1 + soma2;
-	return (resultado); 
+	return resultado; 
 }
-int subtracao (int sub1, int sub2){						/* Área destinada a subtração */
-	int resultado;
+float subtracao (float sub1, float sub2){						/* Área destinada a subtração */
+	float resultado;
 	resultado = sub1 - sub2;
-	return (resultado);
+	return resultado;
 }
-int multiplicacao (int mult1, int mult2){					/* Área destinada a multiplicação */
-	int resultado;
+float multiplicacao (float mult1, float mult2){					/* Área destinada a multiplicação */
+	float resultado;
 	resultado = mult1 * mult2;
-	return (resultado);
+	return resultado;
 }
-int divisao (int div1, int div2){						/* Área destinada a divisão */
-	int resultado;
+float divisao (float div1, float div2){						/* Área destinada a divisão */
+	float resultado;
 	resultado = div1 / div2; 
 	return (resultado);
 }
@@ -143,22 +145,22 @@ int restdiv (int restdiv1, int restdiv2){					/* Área destinada a Resto da Divi
 	resultado = restdiv1 % restdiv2;
 	return (resultado);
 }
-int fatorial (int numfat){							/* Área destinada a Fatorial */
-	int resultado, fat;
+float fatorial (float numfat){							/* Área destinada a Fatorial */
+	float resultado, fat;
 	resultado = 1;
 	for (fat=1;fat<=numfat;fat++)
 	return (resultado);
 }
-int potencia (int base, int expoente){						/* Área destinada a Potenciação */
-	int resultado;
+float potencia (float base, int expoente){						/* Área destinada a Potenciação */
+	float resultado;
 	if (expoente == 0)
 	return 1;
 	else if (expoente == 1)
 	return base;
 	return resultado = base * potencia (base, expoente-1);
 }
-int porcent (int porc1, int porc2){						/* Área destinada a Porcentagem */
-	int resultado;
+float porcent (float porc1, float porc2){						/* Área destinada a Porcentagem */
+	float resultado;
 	resultado = (porc1 * porc2) / 100;
 	return (resultado);
 }
